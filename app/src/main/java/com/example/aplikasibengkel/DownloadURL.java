94aea5e0fd6bdfdd515e8aff316aa4af23a2dd2d
sunggana <94515200+sunggana@users.noreply.github.com>
2022-06-25 19:54:53 +0700

    Update DownloadURL.java

--- app/src/main/java/com/example/aplikasibengkel/DownloadURL.java
+++ app/src/main/java/com/example/aplikasibengkel/DownloadURL.java
@@ -7,39 +7,43 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DownloadURL {
 
-    public String ngambilURL(String url) throws IOException{
-        String urlData = "";
-        HttpURLConnection httpURLConnection = null;
-        InputStream inputStream = null;
+    public String ngambilURL(String myUrl) throws IOException{
+    String data = "";
+    InputStream inputStream = null;
+    HttpURLConnection urlConnection = null;
+    try{
+        URL url = new URL(myUrl);
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.connect();
 
-        try{
-            URL getUrl = new URL(url);
-            httpURLConnection = (HttpURLConnection) getUrl.openConnection();
-            httpURLConnection.connect();
+        inputStream = urlConnection.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuffer sb = new StringBuffer();
 
-            inputStream=httpURLConnection.getInputStream();
-            //read input stream
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuffer sb = new StringBuffer();
+        String line = "";
+        while((line = br.readLine())!=null){
+            sb.append(line);
 
-            String line = "";
-
-            while((line = bufferedReader.readLine())!=null){
-                sb.append(line);
-            }
-            urlData = sb.toString();
-            bufferedReader.close();
-        }catch (Exception e){
-
-            Log.d("Exception",e.toString());
-        }finally{
-            inputStream.close();
-            httpURLConnection.disconnect();
         }
-        return urlData;
+        data = sb.toString();
+        br.close();
+    }catch (MalformedURLException e){
+        e.printStackTrace();
+    }catch (IOException e){
+        e.printStackTrace();
     }
+    finally {
+        if (inputStream != null) {
+            inputStream.close();
+            urlConnection.disconnect();
+        }
+    }
+    return data;
+    }
+
 }
